c9dac513e7dcdd380a0bf970e999304236afd8f9
kimyunn <yunkim.dev@gmail.com>
2025-11-23 17:45:30 +0900

    chore: 프로젝트 초기 설정 - Spring Boot 3.5.7 - Java 21 - Gradle

---
+++ src/test/java/com/epik/EpikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
